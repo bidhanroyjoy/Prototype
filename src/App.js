c34ade51644e06085c5626525d8c0af20eaa5caf
bidhanroyjoy <bidhan19994@gmail.com>
2022-09-07 11:01:56 +0600

    format maintain

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import { useState, useEffect } from "react";
-import { Routes, Route,Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Registration from "./Pages/Registration";
 import Header from "./components/Header/Header";
 import Gallery from "./components/Gallery/Gallery";
 import Footer from "./Pages/Footer";
 import "./App.css";
 
-
 function App() {
   const [active, setActive] = useState("home");
   return (
     <div className="App">
-      <Header />  
+      <Header />
       <Gallery />
       <Registration />
-      <Footer />  
+      <Footer />
       <Routes>
         {/* <Route path="/gallery" element={<Gallery />} /> */}
         {/* <Route path="/registration" element={<Registration setActive={setActive}/>} />   */}
-        {/* <Route path="/footer" element={<Footer />} /> */}     
+        {/* <Route path="/footer" element={<Footer />} /> */}
       </Routes>
     </div>
   );
--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,45 +1,45 @@
-import React from 'react'
-import memstripe from '../../assets/mem_stripe_circle2.svg'
-import memdot from '../../assets/mem_dots_rect2.svg'
-import sparkles from '../../assets/Sparkles.svg'
-import stripe from '../../assets/mem_stipe_square.svg'
-import wave from '../../assets/mem_wave_7.svg'
+import React from "react";
+import memstripe from "../../assets/mem_stripe_circle2.svg";
+import memdot from "../../assets/mem_dots_rect2.svg";
+import sparkles from "../../assets/Sparkles.svg";
+import stripe from "../../assets/mem_stipe_square.svg";
+import wave from "../../assets/mem_wave_7.svg";
 
-import imgleft from '../../assets/Image-Left.png'
-import imgright from '../../assets/Image-Top-Right.png'
-import bottomright from '../../assets/Image-Bottom-Right.png'
-
-
+import imgleft from "../../assets/Image-Left.png";
+import imgright from "../../assets/Image-Top-Right.png";
+import bottomright from "../../assets/Image-Bottom-Right.png";
 
 const Gallery = () => {
   return (
     <div>
-           <div class="bg-tweaks">
+      <div class="bg-tweaks">
         <img class="stripe-2" src={memstripe} alt="" />
         <img class="dot-2" src={memdot} alt="" />
         <img class="sparkles-1" src={sparkles} alt="" />
         <img class="square-stripe-1" src={stripe} alt="" />
         <img class="mem-wave-7" src={wave} alt="" />
-    </div>
-    <section class="glimps">
-            <h1 class="glimps-title">Glimpses</h1>
-            <p class="glimps-subtitle">Our workplace can be best previewed in photos</p>
-            <div class="glimps-gallery">
-                <div class="glimps-gallery-left">
-                    <img src={imgleft} alt="" />
-                </div>
-                <div class="glimps-gallery-left">
-                    <div class="glimps-gallery-left-top">
-                        <img src={imgright} alt="" />
-                    </div>
-                    <div class="glimps-gallery-left-bottom">
-                        <img src={bottomright} alt="" />
-                    </div>
-                </div>
+      </div>
+      <section class="glimps">
+        <h1 class="glimps-title">Glimpses</h1>
+        <p class="glimps-subtitle">
+          Our workplace can be best previewed in photos
+        </p>
+        <div class="glimps-gallery">
+          <div class="glimps-gallery-left">
+            <img src={imgleft} alt="" />
+          </div>
+          <div class="glimps-gallery-left">
+            <div class="glimps-gallery-left-top">
+              <img src={imgright} alt="" />
             </div>
-        </section>
+            <div class="glimps-gallery-left-bottom">
+              <img src={bottomright} alt="" />
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default Gallery
+export default Gallery;
